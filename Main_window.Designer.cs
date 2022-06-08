f669dc34ee9bcd23be23693089555f579e0052ae
TobanCZ <tobiasgameing6@gmail.com>
2022-06-08 21:15:26 +0200

    main

---
+++ Main_window.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Doube_Pendulum
+{
+    partial class Main_window
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.a1_slider = new System.Windows.Forms.TrackBar();
+            this.a2_slider = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.m2_slider = new System.Windows.Forms.TrackBar();
+            this.m1_slider = new System.Windows.Forms.TrackBar();
+            this.r2_slider = new System.Windows.Forms.TrackBar();
+            this.r1_slider = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.a1_slider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.a2_slider)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.m2_slider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m1_slider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r2_slider)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r1_slider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Timer
+            // 
+            this.Timer.Enabled = true;
+            this.Timer.Interval = 15;
+            this.Timer.Tick += new System.EventHandler(this.Update);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1335, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Stop);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1479, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Start);
+            // 
+            // a1_slider
+            // 
+            this.a1_slider.Location = new System.Drawing.Point(40, 26);
+            this.a1_slider.Maximum = 200;
+            this.a1_slider.Minimum = -200;
+            this.a1_slider.Name = "a1_slider";
+            this.a1_slider.Size = new System.Drawing.Size(285, 90);
+            this.a1_slider.TabIndex = 2;
+            this.a1_slider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.a1_slider.Value = 100;
+            this.a1_slider.ValueChanged += new System.EventHandler(this.a1_change);
+            // 
+            // a2_slider
+            // 
+            this.a2_slider.Location = new System.Drawing.Point(40, 97);
+            this.a2_slider.Maximum = 200;
+            this.a2_slider.Minimum = -200;
+            this.a2_slider.Name = "a2_slider";
+            this.a2_slider.Size = new System.Drawing.Size(285, 90);
+            this.a2_slider.TabIndex = 8;
+            this.a2_slider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.a2_slider.Value = 150;
+            this.a2_slider.ValueChanged += new System.EventHandler(this.a2_change);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.m2_slider);
+            this.panel1.Controls.Add(this.m1_slider);
+            this.panel1.Controls.Add(this.r2_slider);
+            this.panel1.Controls.Add(this.r1_slider);
+            this.panel1.Controls.Add(this.a2_slider);
+            this.panel1.Controls.Add(this.a1_slider);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1625, 1649);
+            this.panel1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1198, 26);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 40);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Clear);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(742, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Mass";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(466, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Lenght";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Angle";
+            // 
+            // m2_slider
+            // 
+            this.m2_slider.Location = new System.Drawing.Point(639, 97);
+            this.m2_slider.Maximum = 200;
+            this.m2_slider.Minimum = 5;
+            this.m2_slider.Name = "m2_slider";
+            this.m2_slider.Size = new System.Drawing.Size(285, 90);
+            this.m2_slider.TabIndex = 12;
+            this.m2_slider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.m2_slider.Value = 30;
+            this.m2_slider.ValueChanged += new System.EventHandler(this.m2_Change);
+            // 
+            // m1_slider
+            // 
+            this.m1_slider.Location = new System.Drawing.Point(639, 26);
+            this.m1_slider.Maximum = 200;
+            this.m1_slider.Minimum = 5;
+            this.m1_slider.Name = "m1_slider";
+            this.m1_slider.Size = new System.Drawing.Size(285, 90);
+            this.m1_slider.TabIndex = 11;
+            this.m1_slider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.m1_slider.Value = 30;
+            this.m1_slider.ValueChanged += new System.EventHandler(this.m1_change);
+            // 
+            // r2_slider
+            // 
+            this.r2_slider.Location = new System.Drawing.Point(348, 97);
+            this.r2_slider.Maximum = 200;
+            this.r2_slider.Minimum = 1;
+            this.r2_slider.Name = "r2_slider";
+            this.r2_slider.Size = new System.Drawing.Size(285, 90);
+            this.r2_slider.TabIndex = 10;
+            this.r2_slider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.r2_slider.Value = 150;
+            this.r2_slider.ValueChanged += new System.EventHandler(this.r2_change);
+            // 
+            // r1_slider
+            // 
+            this.r1_slider.Location = new System.Drawing.Point(348, 26);
+            this.r1_slider.Maximum = 200;
+            this.r1_slider.Minimum = 1;
+            this.r1_slider.Name = "r1_slider";
+            this.r1_slider.Size = new System.Drawing.Size(285, 90);
+            this.r1_slider.TabIndex = 9;
+            this.r1_slider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.r1_slider.Value = 150;
+            this.r1_slider.ValueChanged += new System.EventHandler(this.r1_change);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1625, 1649);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Double Pendulum";
+            ((System.ComponentModel.ISupportInitialize)(this.a1_slider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.a2_slider)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.m2_slider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.m1_slider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r2_slider)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.r1_slider)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar a1_slider;
+        private System.Windows.Forms.TrackBar a2_slider;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar m2_slider;
+        private System.Windows.Forms.TrackBar m1_slider;
+        private System.Windows.Forms.TrackBar r2_slider;
+        private System.Windows.Forms.TrackBar r1_slider;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Main_window.cs
@@ -0,0 +1,157 @@
+﻿//https://www.youtube.com/watch?v=uWzPe_S-RVE
+//Tobias Ondra 2022
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Doube_Pendulum
+{
+    public partial class Main_window : Form
+    {
+        Graphics panel_gr;
+        Graphics path_gr;
+        Pen path_pen;
+        Pen pen;
+        SolidBrush brush;
+        Bitmap path;
+
+        float y_offset = 150;
+        float prev_x = -1;
+        float prev_y = -1;
+
+        double r1 = 150; //radius
+        double r2 = 150;
+        double m1 = 30; //mass
+        double m2 = 30;
+        double a1 = 1; //angle
+        double a2 = 1.5;
+        double a1_v = 0; //velocity
+        double a2_v = 0;
+        double a1_a = 0; //akcelerace
+        double a2_a = 0;
+        double g = 1; //gravity
+
+        public Main_window()
+        {
+            InitializeComponent();
+            panel_gr = panel1.CreateGraphics();
+            path = new Bitmap(panel1.Width, panel1.Height);
+            path_gr = Graphics.FromImage(path);
+            path_gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            pen = new Pen(Color.Black, 2);
+            path_pen = new Pen(Color.Black, 1);
+            brush = new SolidBrush(Color.Black);
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            formula();
+            a1_v += a1_a;
+            a2_v += a2_a;
+            a1 += a1_v;
+            a2 += a2_v;
+            Draw();
+        }
+
+        private void Draw() //Nakresleni kyvadel podle uhlu
+        {
+            Bitmap tmp = new Bitmap(panel1.Width, panel1.Height);
+            Graphics gr = Graphics.FromImage(tmp);
+            gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            gr.Clear(Color.White);
+            gr.DrawImage(path,0,0);
+            float x1 = (float)r1 * (float)Math.Sin(a1);
+            float y1 = (float)r1 * (float)Math.Cos(a1);
+            float x2 = (float)r2 * (float)Math.Sin(a2);
+            float y2 = (float)r2 * (float)Math.Cos(a2);
+            gr.DrawLine(pen, panel1.Width/2, y_offset, panel1.Width / 2 + x1, y_offset + y1);
+            gr.FillEllipse(brush, panel1.Width / 2 + x1 - (float)m1/2, y_offset + y1 - (float)m1 /2, (float)m1, (float)m1);
+            gr.DrawLine(pen, panel1.Width / 2 + x1, y_offset + y1, panel1.Width / 2 + x2 + x1, y_offset + y1 +y2);
+            gr.FillEllipse(brush, panel1.Width / 2 + x2 + x1 - (float)m2 /2, y_offset + y1 + y2 - (float)m2 /2, (float)m2, (float)m2);
+            panel_gr.DrawImage(tmp,0,0);
+            gr.Dispose();
+            tmp.Dispose();
+
+            var x = panel1.Width / 2 + x2 + x1;
+            var y = y_offset + y1 + y2;
+            if (prev_x != -1 && Timer.Enabled == true)
+            {
+                path_gr.DrawLine(path_pen, prev_x, prev_y, x, y);
+            }
+
+            prev_x = x;
+            prev_y = y;
+        }
+
+        private void formula() //rovnice ktere nerozumim
+        {
+            var c1 = -g * (2 * m1 + m2) * Math.Sin(a1); //čitatel
+            var c2 = -m2 * g * Math.Sin(a1 - 2 * a2);
+            var c3 = -2 * Math.Sin(a1 - a2) * m2;
+            var c4 = a2_v * a2_v * r2 + a1_v * a1_v * r1 * Math.Cos(a1 - a2);
+            var j = r1 *(2 * m1 + m2 - m2 * Math.Cos(2 * a1 - 2 * a2)); //jmenovatel
+            a1_a = (c1 + c2 + c3 * c4) / j;
+
+            c1 = 2 * Math.Sin(a1 - a2);
+            c2 = a1_v * a1_v * r1 * (m1 + m2);
+            c3 = g * (m1 + m2) * Math.Cos(a1);
+            c4 = a2_v * a2_v * r2 * m2 * Math.Cos(a1 - a2);
+            j = r2 * (2 * m1 + m2 - m2 * Math.Cos(2 * a1 - 2 * a2));
+            a2_a = (c1 * (c2+c3+c4) ) / j;
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            Timer.Start();
+        }
+
+        private void Stop(object sender, EventArgs e)
+        {
+            Timer.Stop();
+            path_gr.Clear(Color.White);
+            Draw();
+        }
+
+        private void a1_change(object sender, EventArgs e)
+        {
+            a1 = (float)a1_slider.Value/100;
+            Draw();
+        }
+
+        private void a2_change(object sender, EventArgs e)
+        {
+            a2 = (float)a2_slider.Value/100;
+            Draw();
+        }
+
+        private void r2_change(object sender, EventArgs e)
+        {
+            r2 = r2_slider.Value;
+            Draw();
+        }
+
+        private void r1_change(object sender, EventArgs e)
+        {
+            r1 = r1_slider.Value;
+            Draw();
+        }
+
+        private void m1_change(object sender, EventArgs e)
+        {
+            m1 = m1_slider.Value;
+            Draw();
+        }
+
+        private void m2_Change(object sender, EventArgs e)
+        {
+            m2 = m2_slider.Value;
+            Draw();
+        }
+
+        private void Clear(object sender, EventArgs e)
+        {
+            path_gr.Clear(Color.White);
+        }
+    }
+}
